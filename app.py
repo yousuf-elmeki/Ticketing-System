35c74b024369b1e8a66b34c209638722a3c2ae0b
yousuf-elmeki <yousuf.elmeki@ontariotechu.net>
2026-04-06 18:24:56 -0400

    added update and delete routes

--- app.py
+++ app.py
@@ -34,12 +34,12 @@ def tickets():
     cursor = db.cursor(dictionary=True)
 
     cursor.execute("""
-        SELECT t.title, t.description, u.full_name,
-               s.status_label, p.priority_level
-        FROM Ticket t
-        JOIN User u ON t.user_id = u.user_id
-        JOIN Status s ON t.status_id = s.status_id
-        JOIN Priority p ON t.priority_id = p.priority_id
+        SELECT t.ticket_id, t.title, t.description,
+       u.full_name, s.status_label, p.priority_level
+FROM Ticket t
+JOIN User u ON t.user_id = u.user_id
+JOIN Status s ON t.status_id = s.status_id
+JOIN Priority p ON t.priority_id = p.priority_id
     """)
 
     tickets = cursor.fetchall()
@@ -74,6 +74,59 @@ def create_ticket():
 
     return render_template("create.html")
 
+
+
+@app.route("/update/<int:ticket_id>", methods=["GET", "POST"])
+def update_ticket(ticket_id):
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+
+    if request.method == "POST":
+        status_id = request.form["status"]
+        priority_id = request.form["priority"]
+
+        cursor.execute("""
+            UPDATE Ticket
+            SET status_id = %s, priority_id = %s
+            WHERE ticket_id = %s
+        """, (status_id, priority_id, ticket_id))
+
+        db.commit()
+        db.close()
+
+        return redirect("/tickets")
+
+    # GET request
+    cursor.execute("SELECT * FROM Ticket WHERE ticket_id = %s", (ticket_id,))
+    ticket = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM Status")
+    statuses = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM Priority")
+    priorities = cursor.fetchall()
+
+    db.close()
+
+    return render_template(
+        "update.html",
+        ticket=ticket,
+        statuses=statuses,
+        priorities=priorities
+    )
+
+
+@app.route("/delete/<int:ticket_id>")
+def delete_ticket(ticket_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+
+    cursor.execute("DELETE FROM Ticket WHERE ticket_id = %s", (ticket_id,))
+    db.commit()
+
+    db.close()
+
+    return redirect("/tickets")
 
 # -------- RUN APP --------
 if __name__ == "__main__":
