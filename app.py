aaa49d7f70f367503eaa35da1dc9c2cba989fb3e
yousuf-elmeki <yousuf.elmeki@ontariotechu.net>
2026-04-06 16:41:25 -0400

    Add MySQL connection and tickets route with JOIN query

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import mysql.connector
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+conn = mysql.connector.connect(
+    host="54.84.46.182",
+    user="root",
+    password="password",
+    database="helpdesk"
+)
 
 @app.route("/")
 def home():
@@ -13,6 +21,21 @@ def test():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/tickets")
+def tickets():
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("""
+    SELECT t.ticket_id, t.title, u.full_name, s.status_label, p.priority_level
+    FROM Ticket t
+    JOIN User u ON t.user_id = u.user_id
+    JOIN Status s ON t.status_id = s.status_id
+    JOIN Priority p ON t.priority_id = p.priority_id
+    """)
+
+    data = cursor.fetchall()
+    return str(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
