ca1516701f8ad414640786b879433e114de7fc7b
yousuf-elmeki <yousuf.elmeki@ontariotechu.net>
2026-04-06 17:35:46 -0400

    added ticket tables

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Database connection
-conn = mysql.connector.connect(
-    host="44.193.107.126",
-    user="root",
-    password="password",
-    database="helpdesk"
-)
+# Config details
+db_config = {
+    "host": "44.193.107.126",
+    "user": "root",
+    "password": "password",
+    "database": "helpdesk"
+}
+
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
 
 @app.route("/")
 def home():
@@ -25,17 +28,25 @@ def about():
 
 @app.route("/tickets")
 def tickets():
-    cursor = conn.cursor(dictionary=True)
+    # 1. Connect to the DB
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
 
+    # 2. Execute the join query
     cursor.execute("""
-    SELECT t.ticket_id, t.title, t.description, u.full_name, s.status_label, p.priority_level
-    FROM Ticket t
-    JOIN User u ON t.user_id = u.user_id
-    JOIN Status s ON t.status_id = s.status_id
-    JOIN Priority p ON t.priority_id = p.priority_id
+        SELECT t.ticket_id, t.title, t.description, u.full_name, 
+               s.status_label, p.priority_level
+        FROM Ticket t
+        JOIN User u ON t.user_id = u.user_id
+        JOIN Status s ON t.status_id = s.status_id
+        JOIN Priority p ON t.priority_id = p.priority_id
     """)
 
     tickets_data = cursor.fetchall()
+    
+    # 3. Close the "worker" and the connection
+    cursor.close()
+    db.close()
 
     return render_template("tickets.html", tickets=tickets_data)
 
